46d33288029dadcf6d34a5752826f5793aafe724
TheExiledCat <43574582+TheExiledCat@users.noreply.github.com>
2019-11-25 12:45:19 +0100

    started on navigation

---
+++ VS1 MV/Assets/Node.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node : MonoBehaviour
+{
+
+    public Collider[] closestNodes;
+    [SerializeField]
+    float radius =1;
+    public LayerMask nodes;
+   public  Vector3 Position
+    {
+        get
+        {
+            return transform.position;
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        closestNodes = Physics.OverlapSphere(transform.position, radius,nodes);
+     
+    }
+    void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(transform.position, radius);
+    }
+    public Collider[] GetNodes()
+    {
+        Collider[] arr = { closestNodes[1], closestNodes[2] };
+        return arr;
+    }
+}
---
+++ VS1 MV/Assets/Pathfinding.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pathfinding : MonoBehaviour
+{
+    Node current;
+    Node target;
+    Node closer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        closer = null;
+    }
+    void FindPath()
+    {
+        if (target != null)
+        {
+            Node next;
+         
+            for (int i = 0; i < current.GetNodes().Length; i++)
+            {
+                if(Vector3.Distance(current.GetNodes()[i].transform.position, target.Position) < Vector3.Distance(closer.transform.position, target.Position){
+
+                }
+            }
+            
+
+
+
+
+        }
+     
+    }
+}
